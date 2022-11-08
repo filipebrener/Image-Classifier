595e9c25d7246357d0a8303b19cc3ef56717fdc3
yanvzp@gmail.com <yanvzp@gmail.com>
2022-11-07 21:24:31 -0300

    preparo pro inicio da implementação

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Image-Classifier
 Classificador de imagem desenvolvido em python para ganho de créditos na disciplina SIN-393 Visão Computacional do professor João Fernando Mari da Universidade Federal de Viçosa
+
+## trem q deu errado
+
+sklearn tá na versão -> 0.0.post1 | era pra ta na 1.1.3 btf
---
+++ main..py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy import spatial, stats
+from sklearn import datasets, model_selection, preprocessing, neighbors, metrics, svm, naive_bayes
+
+import matplotlib.pylab as plt
+from matplotlib.colors import ListedColormap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import seaborn as sns
+import pandas as pd
